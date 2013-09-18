c11d331e67e0cb66e42571849b3acb5b5e04c4c9
Marcos Ramos <ms.ramos@outlook.com>
2013-09-18 14:57:25 -0300

    video_plugin: Create video blocks: added support to web videos.
    (ActionItem2823)
    
    Signed-off-by: Arthur Del Esposte <arthurmde@yahoo.com.br>
    Signed-off-by: Daniel Bucher <daniel.bucher88@gmail.com>
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>
    Signed-off-by: Carlos André <carlos.andre.souza@msn.com>
    Signed-off-by: Luiz Matos <luizff.matos@gmail.com>
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>
    Signed-off-by: Valessio Brito <contato@valessiobrito.com.br>

--- app/models/video_block.rb
+++ app/models/video_block.rb
@@ -1,6 +1,39 @@
 class VideoBlock < Block
 
   settings_items :url, :type => :string, :default => ""
+  
+  def is_youtube? 
+    false
+
+    if url.include?("youtube.com")
+      true
+    end
+  end
+
+  def is_vimeo? 
+    false
+
+    if url.include?("vimeo.com")
+      true
+    end
+  end
+
+  def is_file? 
+    false
+
+    extensions = [".mp4", ".ogg", ".ogv", ".mp3", ".wmv"]
+    if extensions.include? url[-4, 4]
+      true
+    end
+  end
+
+  def format_embed_video_url_for_youtube
+    self.url.gsub("watch?v=", "embed/")
+  end
+
+  def format_embed_video_url_for_vimeo
+    self.url.gsub("vimeo.com/", "player.vimeo.com/video/")
+  end
 
   def self.description
     _('Add Video')
@@ -18,6 +51,7 @@ class VideoBlock < Block
     end
   end
 
+  
   def cacheable?
     false
   end
