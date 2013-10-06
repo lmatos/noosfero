19e81f03956cf7e03bf5dccc06ea4041cc18d12d
tales martins de souza <tales@tales.Cisco>
2013-10-06 07:18:52 -0300

    join_button: Join/Leave/Login button added at members block and adding checkbox to show or not the buttons on the community members
    (ActionItem2844)
    
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>
    Signed-off-by: David Carlos <ddavidcarlos1392@gmail.com>
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>
    Signed-off-by: Tales Martins <tales.martins@gmail.com>
    Signed-off-by: Leandro Alves <leandrosustenido@gmail.com>

--- app/models/members_block.rb
+++ app/models/members_block.rb
@@ -1,5 +1,7 @@
 class MembersBlock < ProfileListBlock
 
+  settings_items :show_join_leave_button, :type => :boolean, :default => false
+  
   def self.description
     _('Members')
   end
@@ -14,13 +16,80 @@ class MembersBlock < ProfileListBlock
 
   def footer
     profile = self.owner
+    show_button_block = show_join_leave_button
     lambda do
-      link_to _('View all'), :profile => profile.identifier, :controller => 'profile', :action => 'members'
+         data = []
+	       data.push(link_to _('View all'), :profile => profile.identifier, :controller => 'profile', :action => 'members')
+
+         
+		if show_button_block     
+		  if logged_in?
+            if profile.members.include?(user)
+	            data.push(button(:delete, content_tag('span',  __('Leave community')), profile.leave_url,
+	              :class => 'leave-community',
+	              :title => _("Leave community"),
+	              :style => 'position: relative;'))
+	            data.push(button(:add, content_tag('span', __('Join')), profile.join_url,
+	              :class => 'join-community',
+	              :title => _("Join community"),
+	              :style => 'position: relative; display: none;'))
+            else
+	            data.push(button(:delete, content_tag('span',  __('Leave community')), profile.leave_url,
+	              :class => 'leave-community',
+	              :title => _("Leave community"),
+	              :style => 'position: relative; display: none;'))
+	            data.push(button(:add, content_tag('span', __('Join')), profile.join_url,
+	              :class => 'join-community',
+	              :title => _("Join community"),
+	              :style => 'position: relative;'))
+           end
+         else
+           data.push(button(:add, content_tag('span', _('Join')), profile.join_not_logged_url,
+              :class => 'button with-text icon-add',
+              :title => _('Join this community'),
+              :style => 'position: relative;'))
+        end
+    end
+       
+    data
+    
+
     end
   end
 
   def profiles
     owner.members
   end
+
+  def options
+    data = ["<br />"]
+    data.push "<input type='checkbox' value='1' name='block[show_join_leave_button]' id='block_show_join_leave_button' "+check_join_leave_button?+" />"
+    data.push "<label for='block_show_join_leave_button'>"+_("show join leave button")+"</labe>"
+    data.push "<input type='hidden' value='"+save_join_leave_button?+"' name='block[show_join_leave_button]'/>"
+
+    data
+  end
+
+  # DELETE-ME, apenas para testar check_join_leave_button? via console
+  def teste_check
+    check_join_leave_button?
+  end
+
+  private
+    def check_join_leave_button?
+      if show_join_leave_button
+        "checked='checked'"
+      else
+        ""
+      end
+    end
+
+    def save_join_leave_button?
+      if show_join_leave_button
+        "0"
+      else
+        "1"
+      end
+    end
 
 end
