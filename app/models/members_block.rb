fd65f1e4f537b46d31d9b0fe9adb5355b05f552e
tales martins de souza <tales@tales.Cisco>
2013-10-06 07:18:52 -0300

    Join/Leave/Login button added at members block.
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>
    Signed-off-by: Tales Martins <tales.martins@gmail.com>

--- app/models/members_block.rb
+++ app/models/members_block.rb
@@ -1,5 +1,7 @@
 class MembersBlock < ProfileListBlock
 
+  settings_items :show_join_leave_button, :type => :boolean, :default => false
+  
   def self.description
     _('Members')
   end
@@ -15,12 +17,50 @@ class MembersBlock < ProfileListBlock
   def footer
     profile = self.owner
     lambda do
-      link_to _('View all'), :profile => profile.identifier, :controller => 'profile', :action => 'members'
+         data = []
+	 data.push(link_to _('View all'), :profile => profile.identifier, :controller => 'profile', :action => 'members')
+
+         if logged_in?
+           if profile.members.include?(user)
+            data.push(button(:delete, content_tag('span',  __('Leave community')), profile.leave_url,
+              :class => 'leave-community',
+              :title => _("Leave community"),
+              :style => 'position: relative;'))
+            data.push(button(:add, content_tag('span', __('Join')), profile.join_url,
+              :class => 'join-community',
+              :title => _("Join community"),
+              :style => 'position: relative; display: none;'))
+           else
+            data.push(button(:delete, content_tag('span',  __('Leave community')), profile.leave_url,
+              :class => 'leave-community',
+              :title => _("Leave community"),
+              :style => 'position: relative; display: none;'))
+            data.push(button(:add, content_tag('span', __('Join')), profile.join_url,
+              :class => 'join-community',
+              :title => _("Join community"),
+              :style => 'position: relative;'))
+          end
+        else
+           data.push(button(:add, content_tag('span', _('Join')), profile.join_not_logged_url,
+              :class => 'button with-text icon-add',
+              :title => _('Join this community'),
+              :style => 'position: relative;'))
+        end
+
+         data
     end
   end
 
   def profiles
     owner.members
   end
+
+  def options
+    data = []
+    #data.push(checkbox())
+    #data.push(_("show join leave button"))
+     
+  end
+
 
 end
