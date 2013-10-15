34c619c1fe4532f3ad75c12db081b26c2f39a1e1
Arthur Del Esposte <arthurmde@yahoo.com.br>
2013-10-15 17:42:48 +0000

    join_button: Cucumber tests for join/leave button in members block, Unit tests for members block
    (ActionItem2844)
    
    Signed-off-by: Arthur Del Esposte <arthurmde@yahoo.com.br>
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>
    Signed-off-by: Leandro Alves <leandrosustenido@gmail.com>
    Signed-off-by: Tales Martins <tales.martins@gmail.com>

--- test/unit/members_block_test.rb
+++ test/unit/members_block_test.rb
@@ -23,7 +23,7 @@ class MembersBlockTest < ActiveSupport::TestCase
     expects(:_).with('View all').returns('View all')
     expects(:link_to).with('View all' , :profile => 'mytestuser', :controller => 'profile', :action => 'members').returns('link-to-members')
 
-    assert_equal 'link-to-members', instance_eval(&block.footer)
+    assert_equal 'link-to-members', instance_eval(&block.footer)[0]
   end
 
   should 'pick random members' do
@@ -38,5 +38,58 @@ class MembersBlockTest < ActiveSupport::TestCase
     assert_same list, block.profiles
   end
 
-end
+  should "options return br, checkbox, label and hidden" do
+    block = MembersBlock.new
 
+    assert_equal "<br />", block.options[0]
+    assert block.options[1].include? "input type='checkbox'"
+    assert block.options[2].include? "label for"
+    assert block.options[3].include? "input type='hidden'"
+  end
+
+  should "check_join_leave_button? return checked if show_join_leave_button is true" do
+    block = MembersBlock.new
+    block.show_join_leave_button = true
+    
+    assert_equal "checked='checked'", block.send(:check_join_leave_button?)
+  end
+
+  should "check_join_leave_button? does not return checked if show_join_leave_button is false" do
+    block = MembersBlock.new
+    block.show_join_leave_button = false
+    
+    assert_equal "", block.send(:check_join_leave_button?)
+  end
+
+  should "save_join_leave_button? return 1 if show_join_leave_button is false" do
+    block = MembersBlock.new
+    block.show_join_leave_button = false
+    
+    assert_equal "1", block.send(:save_join_leave_button?)
+  end
+
+  should "save_join_leave_button? return 0 if show_join_leave_button is true" do
+    block = MembersBlock.new
+    block.show_join_leave_button = true
+    
+    assert_equal "0", block.send(:save_join_leave_button?)
+  end
+
+  should "footer has only 'View all' if show_join_leave_button is false" do
+    env = fast_create(Environment)
+    profile = create_user('mytestuser').person
+    block = MembersBlock.new
+    block.box = profile.boxes.first
+    block.save!
+  
+    block.stubs(:show_join_leave_button).returns(false)
+    
+    expects(:_).with('View all').returns('View all')
+    expects(:link_to).with('View all' , :profile => 'mytestuser', :controller => 'profile', :action => 'members').returns('link-to-members')
+    
+    link = instance_eval(&block.footer)
+    assert_equal "link-to-members", link[0]
+    assert_equal 1, link.size
+  end
+  
+end
