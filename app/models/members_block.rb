fb4074fc4908134986e99a89849d09ca2e93dd7e
David Carlos <ddavidcarlos1392@gmail.com>
2013-10-15 15:47:01 +0000

    Removing the render button "enter/leave" when the checkbox that enables the button is unchecked
    Signed-off-by: David Carlos <ddavidcarlos1392@gmail.com>
    Signed-off-by: Tales Martins <tales.martins@gmail.com>
    Signed-off-by: Leandro Alves <leandrosustenido@gmail.com>

--- app/models/members_block.rb
+++ app/models/members_block.rb
@@ -16,38 +16,44 @@ class MembersBlock < ProfileListBlock
 
   def footer
     profile = self.owner
+    show_button_block = show_join_leave_button
     lambda do
          data = []
 	       data.push(link_to _('View all'), :profile => profile.identifier, :controller => 'profile', :action => 'members')
 
-         if logged_in?
-           if profile.members.include?(user)
-            data.push(button(:delete, content_tag('span',  __('Leave community')), profile.leave_url,
-              :class => 'leave-community',
-              :title => _("Leave community"),
-              :style => 'position: relative;'))
-            data.push(button(:add, content_tag('span', __('Join')), profile.join_url,
-              :class => 'join-community',
-              :title => _("Join community"),
-              :style => 'position: relative; display: none;'))
-           else
-            data.push(button(:delete, content_tag('span',  __('Leave community')), profile.leave_url,
-              :class => 'leave-community',
-              :title => _("Leave community"),
-              :style => 'position: relative; display: none;'))
-            data.push(button(:add, content_tag('span', __('Join')), profile.join_url,
-              :class => 'join-community',
-              :title => _("Join community"),
-              :style => 'position: relative;'))
-          end
-        else
+         
+		if show_button_block     
+		  if logged_in?
+            if profile.members.include?(user)
+	            data.push(button(:delete, content_tag('span',  __('Leave community')), profile.leave_url,
+	              :class => 'leave-community',
+	              :title => _("Leave community"),
+	              :style => 'position: relative;'))
+	            data.push(button(:add, content_tag('span', __('Join')), profile.join_url,
+	              :class => 'join-community',
+	              :title => _("Join community"),
+	              :style => 'position: relative; display: none;'))
+            else
+	            data.push(button(:delete, content_tag('span',  __('Leave community')), profile.leave_url,
+	              :class => 'leave-community',
+	              :title => _("Leave community"),
+	              :style => 'position: relative; display: none;'))
+	            data.push(button(:add, content_tag('span', __('Join')), profile.join_url,
+	              :class => 'join-community',
+	              :title => _("Join community"),
+	              :style => 'position: relative;'))
+           end
+         else
            data.push(button(:add, content_tag('span', _('Join')), profile.join_not_logged_url,
               :class => 'button with-text icon-add',
               :title => _('Join this community'),
               :style => 'position: relative;'))
         end
+    end
+       
+    data
+    
 
-         data
     end
   end
 
