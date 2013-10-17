6e47b6c2c6c208c1d94eeae00f55e59fab8efc02
Marcos Ramos <ms.ramos@outlook.com>
2013-10-17 17:07:58 -0300

    blog: added cover image for blog
    (ActionItem2849)
    
    Signed-off-by: Marcos Ramos <ms.ramos@outlook.com
    Signed-off-by: Valéssio Brito <contato@valessiobrito.com.br>

---
+++ db/migrate/20131017200051_add_image_id_on_article.rb
@@ -0,0 +1,11 @@
+class AddImageIdOnArticle < ActiveRecord::Migration
+  def self.up
+    add_column :articles, :image_id, :integer
+    add_column :article_versions, :image_id, :integer
+  end
+
+  def self.down
+    remove_column :articles, :image_id
+    remove_column :article_versions, :image_id
+  end
+end
