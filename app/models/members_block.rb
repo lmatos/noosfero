fec31c483c1001df00d5980249b376e0526b183b
Valessio Brito <contato@valessiobrito.info>
2013-10-17 18:10:23 -0300

    Adjust style CSS and display button
    (ActionItem2844)

--- app/models/members_block.rb
+++ app/models/members_block.rb
@@ -1,7 +1,7 @@
 class MembersBlock < ProfileListBlock
 
   settings_items :show_join_leave_button, :type => :boolean, :default => false
-  
+
   def self.description
     _('Members')
   end
@@ -22,35 +22,31 @@ class MembersBlock < ProfileListBlock
       data = []
 	    data.push(link_to _('View all'), :profile => profile.identifier, :controller => 'profile', :action => 'members')
 
-		  if show_button_block     
+		  if show_button_block
 		    if logged_in?
           if profile.members.include?(user)
             data.push(button(:delete, content_tag('span',  __('Leave community')), profile.leave_url,
               :class => 'leave-community',
-	            :title => _("Leave community"),
-	            :style => 'position: relative;'))
+	            :title => _("Leave community")))
             data.push(button(:add, content_tag('span', __('Join')), profile.join_url,
               :class => 'join-community',
               :title => _("Join community"),
-              :style => 'position: relative; display: none;'))
+              :style => 'display: none;'))
           else
             data.push(button(:delete, content_tag('span',  __('Leave community')), profile.leave_url,
               :class => 'leave-community',
               :title => _("Leave community"),
-              :style => 'position: relative; display: none;'))
+              :style => 'display: none;'))
             data.push(button(:add, content_tag('span', __('Join')), profile.join_url,
               :class => 'join-community',
-              :title => _("Join community"),
-              :style => 'position: relative;'))
+              :title => _("Join community")))
           end
         else
           data.push(button(:add, content_tag('span', _('Join')), profile.join_not_logged_url,
             :class => 'button with-text icon-add',
-            :title => _('Join this community'),
-            :style => 'position: relative;'))
+            :title => _('Join this community')))
         end
-      end 
-
+      end
       data
     end
   end
