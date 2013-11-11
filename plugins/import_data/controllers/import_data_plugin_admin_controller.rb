6be93b4e0e9eea8791d3b3bbf29f65196b0ada96
Luiz Fernando de Freitas Matos <luiz@luizff.matos@gmail.com>
2013-11-11 18:27:04 -0200

    import_data_plugin: create configuration and file form upload pages
    (ActionItem2745)
    
    Signed-off-by: Carlos Andre <carlos.andre.souza@msn.com>
    Signed-off-by: Luiz Matos <luizff.matos@gmail.com>

---
+++ plugins/import_data/controllers/import_data_plugin_admin_controller.rb
@@ -0,0 +1,13 @@
+class ImportDataPluginAdminController < AdminController
+
+  append_view_path File.join(File.dirname(__FILE__) + '/../views')
+
+  def index
+  end
+
+  def imṕort_data
+
+  end
+
+end
+
---
+++ plugins/import_data/lib/import_data_plugin.rb
@@ -0,0 +1,11 @@
+class ImportDataPlugin < Noosfero::Plugin
+
+  def self.plugin_name
+    "Import Data"
+  end
+
+  def self.plugin_description
+    _("A plugin that allows import CSV data base.")
+  end
+
+end
