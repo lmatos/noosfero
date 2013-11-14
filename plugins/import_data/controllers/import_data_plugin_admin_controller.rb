eb4491a0eb34f2d61359cb42f56d1aefb081d4cc
Marcos Ramos <ms.ramos@outlook.com>
2013-11-14 11:48:18 -0200

    import_data_plugin: add a handler class for csv files
    (ActionItem2745)
    
    Signed-off-by: Marcos Ramos <ms.ramos@outlook.com>
    Signed-off-by: David Carlos <ddavidcarlos1392@gmail.com>
    Signed-off-by: Fábio Teixeira <fabio1079@gmail.com>

--- plugins/import_data/controllers/import_data_plugin_admin_controller.rb
+++ plugins/import_data/controllers/import_data_plugin_admin_controller.rb
@@ -4,26 +4,17 @@ require 'csv'
 class ImportDataPluginAdminController < AdminController
 
   append_view_path File.join(File.dirname(__FILE__) + '/../views')
-
+  
   def index
+    render 'upload_file'
   end
 
-  def import_data
-    post = UploadedFile.save(params[:upload])
-
-   reader = CSV.open("public/data/csv/Sample.csv",'r') 
-
-      row1 = reader.shift
-      row2 = reader.shift
-      row3 = reader.shift
-
-      attributes = Array.new
-      row2.map{ |m| attributes << m.split(',') }
-
-      attributes.each { |attri| puts attri}
-
-      row2.map
-    #render :text => "File has been uploaded successfully"
+  def select_fields
+    post = params[:upload]['csv']
+    
+    handler = ImportDataPlugin::CSVHandler.new
+    @csv = handler.parse(post.read) 
+    render 'select_fields'
   end
 
 end
---
+++ plugins/import_data/lib/import_data_plugin/csv_handler.rb
@@ -0,0 +1,19 @@
+require 'csv'
+
+class ImportDataPlugin::CSVHandler
+
+  def parse(content)
+      csv = {}
+      reader = CSV.parse(content)
+      
+      csv['header'] = reader.shift
+      csv['rows'] = []
+    
+    reader.each do |row|
+      csv['rows'] << row
+    end
+
+    csv
+  end
+
+end
