fcc87461114b68cab1c3d5f8a3c24900046ba113
Fabio Teixeira <fabio1079@gmail.com>
2013-11-18 17:57:45 -0200

    import_data_plugins: Creating enterprises using csv file
    (ActionItem2745)
    
    Signed-off-by: Arthur Del Esposte <arthurmde@gmail.com>
    Signed-off-by: Gabriela Navarro <navarro1703@gmail.com>
    Signed-off-by: Luiz Matos <luizff.matos@gmail.com>
    Signed-off-by: Marcos Ramos <ms.ramos@outlook.com>
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- plugins/import_data/controllers/import_data_plugin_admin_controller.rb
+++ plugins/import_data/controllers/import_data_plugin_admin_controller.rb
@@ -11,9 +11,12 @@ class ImportDataPluginAdminController < AdminController
 
   def select_fields
     post = params[:upload]['csv']
-    
+    path = File.join("tmp", "import_data.csv")
+    File.open(path, "wb") { |f| f.write(post.read) }
+
     handler = ImportDataPlugin::CSVHandler.new
-    @csv = handler.parse(post.read) 
+    @csv = handler.parse(path) 
+
     render 'select_fields'
   end
 
@@ -26,19 +29,38 @@ class ImportDataPluginAdminController < AdminController
   end
 
   def perform_migration
-      
+      handler = ImportDataPlugin::CSVHandler.new
+      @csv = handler.parse("tmp/import_data.csv")
+      @relations = params[:relations]
+      @error_log = []
+
       @csv["rows"].each do |row|
-        index = 0
-        e = Enterprise.new
-        @csv["header"].each do |csv_field|
-          e.attributes[relations[csv_field]] =  row[index]
-          e.identifier = 54
-          index +=1 
+        begin
+          index = 0
+          e = Enterprise.new
+
+          @csv["header"].each do |csv_field|
+            unless @relations[csv_field].to_s.empty?
+              if @relations[csv_field].to_s == "identifier"
+                e[@relations[csv_field].to_s] = row[index].to_s.to_slug
+              else 
+                e[@relations[csv_field].to_s] = row[index].to_s
+              end
+            end
+            
+            index +=1
+          end
+          
+          e.save
+        rescue
+          @error_log << {
+              :message => e.errors.full_messages,
+              :indentifier => e.identifier
+          }
         end
-        e.save
-        render :text => "re2323sult", :success => true
       end
-  
+      
+      render :text => "ola mundo", :success => true
   end
 
 end
