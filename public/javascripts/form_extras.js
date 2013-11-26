225e3b2d644dcf80fbd462ab5e6c5e7992227f0a
Fabio Teixeira <fabio1079@gmail.com>
2013-11-26 12:01:22 -0200

    character_limit: Remaining characters to suggest an article and refactoring show_remaining_characters function
    (AI2912-character_limit)

--- public/javascripts/form_extras.js
+++ public/javascripts/form_extras.js
@@ -1,14 +1,18 @@
-function show_remaining_characters(formFieldId , maxlength) {
+function show_remaining_characters(formFieldId , maxlength, user_mensage) {
+  user_mensage = (user_mensage == undefined) ? "Remaining Characters:" : user_mensage;
   var span_id = formFieldId+"_max_length"
   var form_field = jQuery("#"+formFieldId);
   var actual_length = maxlength - form_field.val().length;
 
-  form_field.parent().append("<br /><span id='"+span_id+"'>Remaining Characters: " + actual_length+"</span>");
+  form_field.parent().append("<br /><span id='"+span_id+"'>"+user_mensage+" "+actual_length+"</span>");
   jQuery("#"+span_id).css("margin-left", "10px");
 
+  function show_characters() {
+    var text_length = jQuery(this).val().length;
+    jQuery("#"+span_id).html(user_mensage+" "+(maxlength-text_length));
+  }
+
   form_field.attr("maxlength", maxlength);
-  form_field.keypress(function(){
-      var text_length = jQuery(this).val().length;
-    jQuery("#"+span_id).html("Remaining Characters: "+(maxlength-text_length));
-  });
+  form_field.keypress(show_characters);
+  form_field.blur(show_characters);
 }
