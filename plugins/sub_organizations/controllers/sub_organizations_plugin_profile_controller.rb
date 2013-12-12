8ad842ef2384eb9075e2fd5e0e30f775fc0bc71b
Luiz Fernando de Freitas Matos <luiz@luizff.matos@gmail.com>
2013-12-12 14:21:31 -0200

    Adding paginate to parent organization

--- plugins/sub_organizations/controllers/sub_organizations_plugin_profile_controller.rb
+++ plugins/sub_organizations/controllers/sub_organizations_plugin_profile_controller.rb
@@ -23,11 +23,17 @@ class SubOrganizationsPluginProfileController < ProfileController
 
   def parents
     parents = SubOrganizationsPlugin::Relation.parents(profile)
+	  @communities = parents.communities
+    @enterprises = parents.enterprises
+    @full = true
 
-    if params[:type]
-      @organizations = parents
+    if params[:type] 
+      @communities = SubOrganizationsPlugin.limit(@communities)
+      @enterprises = SubOrganizationsPlugin.limit(@enterprises)
+      @full = false
     else
-      @organizations = SubOrganizationsPlugin.limit(parents)
+      @communities = @communities.paginate(:per_page => 12, :page => params[:npage])
+      @enterprises = @enterprises.paginate(:per_page => 12, :page => params[:npage])
     end
 
     render 'related_organizations'
